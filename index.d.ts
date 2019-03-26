6162ab63ddc8955af96ac19b8d59ec7e7fe3f487
lixw1994 <leexw1994@gmail.com>
2019-03-26 15:53:32 +0800

    增加ts头文件

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+import {EventEmitter} from "events";
+
+type Cb0 = () => void;
+type Cb1 = (result: any) => void;
+type Cb2 = (err: null | Error, result: any) => void;
+interface InitParams {
+    host: string;
+    port: string;
+    scheme?: string;                // 默认 ws
+
+    reconnect?: boolean;            // 默认true
+    maxReconnectAttempts?: number;  // 默认10
+    
+    encode?: (reqId: number, route: string, msg: object) => any;
+    decode?: (data: any) => object;
+    encrypt: boolean;
+    handshakeCallback: Cb1;
+
+    user?: any;
+
+}
+
+declare class PomeloClient extends EventEmitter {
+    init(params: InitParams, cb: Cb0): void;
+
+    disconnect(): void;
+
+    request(route: string, msg: any, cb: Cb1): void;
+    
+    notify(route: string, msg: any): void;
+}
+
+export = PomeloClient;
